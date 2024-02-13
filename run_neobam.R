35f86bd8fac5f9eb9d602cd92f8655f7f971b3bb
Nikki Tebaldi <17799906+nikki-t@users.noreply.github.com>
2024-02-13 15:25:16 +0000

    Allow specification of index in commandline arguments

--- run_neobam.R
+++ run_neobam.R
@@ -23,18 +23,8 @@ STAN_FILE = file.path("/app", "neobam", "neobam_stan_engine.stan")
 #' @param reaches_json name of json reach file
 #'
 #' @return list of swot file and sos file
-get_reach_files = function(reaches_json){
-  # Get reach identifier from array environment variable
-  index = strtoi(Sys.getenv("AWS_BATCH_JOB_ARRAY_INDEX")) + 1
-  args = commandArgs(trailingOnly=TRUE)
-
-
-  if (length(args)>=1){
-      reaches_json = file.path(IN_DIR, paste('reaches',strtoi(args[1]),'.json'))
-  } else{
-      reaches_json = file.path(IN_DIR, 'reaches.json')
-  }
-
+get_reach_files = function(reaches_json, index){
+  # Get reach data from index
   json_data = rjson::fromJSON(file=file.path(reaches_json))[[index]]
   return(list(reach_id = json_data$reach_id,
               swot_file = file.path(IN_DIR, "swot", json_data$swot),
@@ -64,8 +54,17 @@ main = function() {
   # Identify reach files to process
   start = Sys.time()
   args = commandArgs(trailingOnly=TRUE)
-  reaches_json = ifelse(identical(args, character(0)), "reaches.json", args[1])
-  io_data = get_reach_files(reaches_json)
+  if (length(args)>=2){
+      index = strtoi(args[1])
+      reaches_json = file.path(input_dir, paste(args[2]))
+  } else if (length(args)>=1) {
+      index = strtoi(args[1])
+      reaches_json = file.path(input_dir, 'reaches.json')
+  } else{
+      index = strtoi(Sys.getenv("AWS_BATCH_JOB_ARRAY_INDEX")) + 1
+      reaches_json = file.path(input_dir, 'reaches.json')
+  }
+  io_data = get_reach_files(reaches_json, index)
 
   # Get Input
   in_data = get_input(io_data$swot_file, io_data$sos_file, io_data$reach_id)
