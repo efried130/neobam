92a5bcb4892c3ee20e698611f8fcf67ab5c83c68
cjgleason <cjgleason@umass.edu>
2024-04-24 14:23:11 -0400

    Add files via upload

---
+++ neobam/generate_priors.R
@@ -0,0 +1,105 @@
+generate_priors=function(parameters){
+
+    setwd(parameters$wd)
+    
+source('prior_functions.R')
+source('neobam_functions.R')
+    
+library(settings, lib.loc = "/nas/cee-water/cjgleason/r-lib/",quietly = TRUE,warn.conflicts=FALSE)
+library(whisker, lib.loc = "/nas/cee-water/cjgleason/r-lib/",quietly = TRUE,warn.conflicts=FALSE)
+
+Wobs=parameters$Wobs
+Sobs=parameters$Sobs
+Hobs=parameters$Hobs
+dAdWobs=parameters$dAdWobs
+dHdWobs=parameters$dHdWobs   
+Wdiffobs=parameters$Wdiffobs
+dAobs=parameters$dAobs
+dHobs=parameters$dHobs
+Qobs=parameters$Qobs
+Q_priors=parameters$Qpriors
+reachid=parameters$reachid
+date=parameters$date
+sample_size=parameters$sample_size
+    
+    #need dAdW and dHdW
+    #dH, dW, and dA are relative to minimum. simple finite difference should work
+
+
+    
+     #if enough data to be subsampled, do that
+      if(nrow(Hobs)>parameters$sample_size){
+          order_frame=data.frame(row=1:nrow(Hobs),nt=rowSums(!is.na(Hobs)))
+          keep_index=order(order_frame$nt, decreasing=TRUE)[1:sample_size]
+          Hobs=Hobs[keep_index,] 
+          Wobs=Wobs[keep_index,] 
+          Qobs=Qobs[keep_index,] 
+          dAobs=dAobs[keep_index,] 
+          dHobs=dHobs[keep_index,] 
+          dWobs=dWobs[keep_index,] 
+      }    
+    
+    #create a bamdata object for the prior generation
+      Qmean=exp(Q_priors$logQ_hat)
+      bamdata = bam_data(h=Hobs,w=Wobs, dA=dAobs,Qhat=as.vector(Qmean),s=Sobs) 
+  #make full BAM priors (increase knowledge content as much as possible)
+  makeBamPriors = function(bamdata,Q_priors){
+    bampriors = bam_priors(bamdata = bamdata, logQ_sd = Q_priors$logQ_sd, variant = "amhg",classification='expert')
+    return(bampriors)
+  }
+  
+    #create other BAM priors (can be adjusted)
+    bampriors = makeBamPriors(bamdata,Q_priors)
+
+    #print(bampriors$other_priors)
+    norm_to_lognorm=function(mu,sigma){
+    lognorm_mu=2*log(mu)-0.5*log(mu^2+sigma^2)
+    lognorm_sigma= log(mu^2 + sigma^2) - 2*(log(mu))
+    return(list('mu'=lognorm_mu,'sigma'=lognorm_sigma))}
+
+
+# #nudge priors
+# #the max width is less than bankfull given the precentile filter.
+# bampriors$river_type_priors$lowerbound_logn= log(0.02)
+# bampriors$river_type_priors$upperbound_logn= log(0.04)# nudging
+# bampriors$river_type_priors$logn_hat=rep(log(0.05),times=nrow(Wobs))
+# bampriors$river_type_priors$logn_sd = rep(2,times=nrow(Hobs)) #bampriors$river_type_priors$logr_hat /2
+    
+# Q_priors$logQ_hat=rep(Q_priors$logQ_hat,times=nrow(Hobs))
+# Q_priors$logQ_sd=rep(Q_priors$logQ_sd,times=nrow(Hobs))
+
+if(any(is.na(c(Q_priors$logQ_sd,Q_priors$logQ_hat)))){return(list('neo_priors'=NA))}
+                
+hasdat=matrix(as.integer(Hobs>0),nrow=nrow(Hobs),ncol=ncol(Hobs))
+ntot=sum(hasdat)
+
+#for stan, NA to zero
+Sobs[is.na(Sobs)]=0
+Hobs[is.na(Hobs)]=0
+Wobs[is.na(Wobs)]=0
+    
+dHobs[is.na(dHobs)]=0
+dAobs[is.na(dAobs)]=0
+dAdWobs[is.na(dAdWobs)]=0
+    dHdWobs[is.na(dHdWobs)]=0
+    Wdiffobs[is.na(Wdiffobs)]=0
+                     
+   
+   
+return(list('neo_priors'=bampriors,
+            'date'=date,
+            'Hobs'=Hobs,
+            'Sobs'=Sobs,
+            'Wobs'=Wobs,
+            'dAdWobs'=dAdWobs,
+            'dHdWobs'=dHdWobs,
+            'Wdiffobs'=Wdiffobs,
+            'dHobs'=dHobs,
+            'dAobs'=dAobs,
+            'reachid'=reachid,
+            'ntot'=ntot,
+            'hasdat'=hasdat,
+            'Qobs'=Qobs,
+            'Q_priors'=Q_priors))
+    
+}
--- neobam/neobam_functions.R
+++ neobam/neobam_functions.R
@@ -1,209 +1,137 @@
-window_mode=function(Wobs,Sobs,date,perc_lower,perc_upper,nt_window,nx_sample,priors){
 
-    # ?Handled valid observation detection in input
-    # Sobsmat= matrix(Sobs,nrow=nrow(Wobs),ncol=ncol(Wobs))
-    # Sobs=Sobsmat
-
-    # #cleanup
-    #   #more cleanup needed- want at least 5 nx for every nt and 5nt for every nx
-    #   index_of_nodata_row=rowSums(Wobs==0) >= (ncol(Wobs)-5)
-    #   Wobs=Wobs[!index_of_nodata_row,]
-    #   Sobs=Sobs[!index_of_nodata_row,]
-    #
-    #   index_of_nodata_col=colSums(Wobs==0) >= (nrow(Wobs)-5)
-    #   index_of_nodata_col[is.na(index_of_nodata_col)] = TRUE
-    #
-    #   Wobs=Wobs[,!index_of_nodata_col]
-    #   Sobs=Sobs[,!index_of_nodata_col]
-    #   date=date[!index_of_nodata_col]
-    #
-    #   #stop if there aren't enough data
-    #   if (nrow(Wobs)<5 || ncol(Wobs) < 5 ) {return(flag=1)}
-
-    #if there are less times than the window size
-    if (ncol(Wobs)<nt_window){
-      hasdat=matrix(as.integer(Wobs>0),nrow=nrow(Wobs),ncol=ncol(Wobs))
-      ntot=sum(hasdat,na.rm=TRUE)
-      W_index=1:ncol(Wobs)
-    } else {
-      W_mean=colMeans(Wobs, na.rm=TRUE)
-      W_percentiles=quantile(W_mean, c(perc_lower,perc_upper), na.rm=TRUE)
-
-      W_index= which(W_mean>=W_percentiles[1] & W_mean<=W_percentiles[2] )
-    }
-    if (length(W_index)>nt_window){
-      #get a random sample of those in that percentile
-      W_index=sample(W_index,nt_window,replace=FALSE)
-    }
-
-    if (nrow(Wobs)>nx_sample){
-      which_rows=sample(1:nrow(Wobs),nx_sample,replace=FALSE)
-    } else {
-      which_rows=1:nrow(Wobs)
-    }
-
-    # Apply window to observations and priors
-    Wobs=Wobs[which_rows,W_index]
-    Sobs=Sobs[which_rows,W_index]
-    hasdat=matrix(as.integer(Wobs>0),nrow=nrow(Wobs),ncol=ncol(Wobs))
-    ntot=sum(hasdat,na.rm=TRUE)
-    priors=lapply(priors, window_prior, which_rows)
-
- return(list('hasdat'=hasdat,'ntot'=ntot,'Wobs'=Wobs,'Sobs'=Sobs,'row_filter'=which_rows,'column_filter'=W_index, 'priors'=priors))
-}
-
-window_prior = function(prior, which_rows) {
-  if (length(prior) > 1) {
-    return(prior[which_rows])
-  } else {
-    return(prior)
-  }
-}
-
-generate_neobam_priors_and_data = function(windowed, Q_priors,
-                               neobam_prep_data,global_params,window_params) {
-  #Q priors assume the user has a list of pre-calculated priors
-  # logQ_hat= Q_priors$logQ_hat[windowed$column_filter] #because we sampled in time
-  logQ_hat = rep(Q_priors$logQ_hat, times=length(windowed$column_filter))
-  lowerbound_logQ= Q_priors$lowerbound_logQ
-  upperbound_logQ=Q_priors$upperbound_logQ
-  # logQ_sd= Q_priors$logQ_sd[windowed$column_filter]
-  logQ_sd = rep(Q_priors$logQ_sd, times=length(windowed$column_filter))
-
-  #performative variables
-  nx=nrow(windowed$Wobs)
-  nt=ncol(windowed$Wobs)
-  ntot=windowed$ntot
-  hasdat=windowed$hasdat
-  Wobs=windowed$Wobs
-  Sobs=windowed$Sobs
-
-neobam_data_and_priors = list(
-
-    Wobs=Wobs,
-    Sobs=Sobs,
-logQ_hat=logQ_hat,
-lowerbound_logQ=lowerbound_logQ,
-upperbound_logQ=upperbound_logQ,
-logQ_sd=logQ_sd,
-
-nx=nx,
-nt=nt,
-ntot=ntot,
-hasdat=hasdat,
-iter=global_params$iter,
-
-Werr_sd=global_params$Werr_sd, #expected uncertainty in width observations
-Serr_sd=global_params$Serr_sd, #expected uncerainty in slope observations
-iter=global_params$iter, #how many iterations in the markov chain?
-sigma_man=matrix(global_params$sigma_man,nrow=nrow(Wobs),ncol=ncol(Wobs)),
-
-logWb_hat = window_params$logWb_hat,
-logWb_sd = window_params$logWb_sd,
-lowerbound_logWb = window_params$lowerbound_logWb,
-upperbound_logWb = window_params$upperbound_logWb,
-
-logDb_hat = window_params$logDb_hat,
-logDb_sd = window_params$logDb_sd,
-lowerbound_logDb = window_params$lowerbound_logDb,
-upperbound_logDb = window_params$upperbound_logDb,
-
-r_hat = window_params$r_hat,
-r_sd = window_params$r_sd,
-lowerbound_r = window_params$lowerbound_r,
-upperbound_r = window_params$upperbound_r,
-
-logn_hat = window_params$logn_hat,
-logn_sd = window_params$logn_sd,
-lowerbound_logn = window_params$lowerbound_logn,
-upperbound_logn = window_params$upperbound_logn
-
-
-  )
-
-
-}
-
-run_neobam = function(neobam_data_and_priors,sourcefile){
-  #run it here
-  library(rstan)
-  rstan_options(auto_write = FALSE)
+run_neobam_stan = function(neobam_data_and_priors,sourcefile){
+    
+  library(BH,quietly=TRUE,warn.conflicts = FALSE)
+  library(rstan,quietly=TRUE,warn.conflicts = FALSE)
   library(dplyr,quietly=TRUE,warn.conflicts = FALSE)
-  library(tidyr, quietly=TRUE,warn.conflicts = FALSE)
+  library(tidyr, lib.loc = "/nas/cee-water/cjgleason/r-lib/",quietly=TRUE,warn.conflicts = FALSE)
   library(stringr,quietly=TRUE,warn.conflicts = FALSE)
+    
+    rstan_options(auto_write = TRUE)
 
+
+    
   return_posterior_mean_sd= function(parameter,stan_output){
-
-
+      
+      
+    
     var_names=gsub("\\[.*\\]" ,"",   row.names(stan_output) )
     parameter_index=!is.na(str_match(var_names,parameter))
     string_length_index=unlist(lapply(var_names,nchar)) == nchar(parameter)
-
+    
     combined_index=which(parameter_index*string_length_index==1)
-
-    posterior_mean=mean(stan_output[combined_index,]$mean)
-    posterior_sd=sqrt(mean(stan_output[combined_index,]$sd^2))
-
+      
+      #print(stan_output[combined_index,])
+    
+    posterior_mean=stan_output[combined_index,]$mean
+    posterior_sd=mean(stan_output[combined_index,]$sd^2)
+    
     return(list('mean'=posterior_mean,'sd'=posterior_sd))
-
+    
   }
+    
+   
+    
+  
+iter=neobam_data_and_priors$iter
+    
 
-    iter=neobam_data_and_priors$iter
-
+        
   fit1 <- stan(
-
+    
     file = sourcefile,  # Stan program
     data = neobam_data_and_priors,    # named list of data
-    chains = 1,             # number of Markov chains
+    chains = 3,             # number of Markov chains
     warmup = floor(0.1*iter),          # number of warmup iterations per chain
     iter = iter,   # total number of iterations per chain
     cores = 1,              # number of cores (could use one per chain)
-    refresh = 0             # no progress shown
-
+    refresh = 0
+    #        boost_lib='/home/cjgleason_umass_edu/R/x86_64-pc-linux-gnu-library/4.0',
+   #   eigen_lib='/home/cjgleason_umass_edu/R/x86_64-pc-linux-gnu-library/4.0'
   )
-
-
-
+  
   output= data.frame(rstan::summary(fit1)$summary)
   #OK! we now need a list of all the posteriors so we can return them to stuff back into the flow law
-  posterior_list=c('r','logn','logWb','logDb')
-
+  # posterior_list=c('r','logn','betaslope','betaint','logbeta','logQ')
+     posterior_list=c('r','logn','logWb','logDb','logQ')
+    
   posteriors=lapply(posterior_list,return_posterior_mean_sd,output)
   names(posteriors)=posterior_list
+    
+    nchains <- fit1@sim$chains
+     chain <- 1:nchains
+      alpha <- 1 - 0.95
+    
+  hydrograph_posterior=  rstan::extract(fit1, "logQ", permuted = FALSE) %>%
+    reshape2::melt()%>%
+    dplyr::mutate(chains = gsub("^chain:", "", .data$chains)) %>%
+    dplyr::filter(.data$chains %in% chain) %>%
+    dplyr::mutate(value = exp(.data$value)) %>%
+    dplyr::group_by(.data$parameters) %>%
+    dplyr::summarize(mean = mean(.data$value),
+              conf.low = quantile(.data$value, alpha / 2),
+              conf.high = quantile(.data$value, 1 - (alpha / 2)),
+              sd = sd(.data$value)) %>%
+    dplyr::rename(time = .data$parameters) %>%
+    dplyr::mutate(time = gsub("^logQ\\[", "", .data$time),
+           time = gsub("\\]$", "", .data$time),
+           time = as.numeric(.data$time)) %>%
+    dplyr::arrange(.data$time)
+    
+    # posteriors$logbeta$mean = (posteriors$r$mean * posteriors$betaslope$mean) + posteriors$betaint$mean
+    # posteriors$logbeta$sd = (posteriors$r$sd * posteriors$betaslope$sd) + posteriors$betaint$sd
 
-  return(posteriors)
+
+  return(list('posteriors'=posteriors,'hydrograph_posterior'=hydrograph_posterior))
 
 }
 
 remake_discharge =function (Wobs,Sobs,posteriors){
+    
+    
+
   r=posteriors$r$mean
-  logWb=posteriors$logWb$mean
-  logDb=posteriors$logDb$mean
   logn=posteriors$logn$mean
+  logbeta=posteriors$logDb$mean -(r*posteriors$logWb$mean)
+  
+    logW=log(Wobs)
+    logS=log(Sobs)
+    
+finalQ=matrix(NA,nrow=nrow(logS),ncol=ncol(logS))
 
 
+    
+for (i in 1:nrow(logS)){
 
-delta=(1.67*r) +1
-logQ=  (delta*log(Wobs))-(1.67*r*logWb)+(1.67*logDb)-(1.67*log((r+1)/r))-(logn)+(0.5*log(Sobs))
-
-logQ[is.infinite(logQ)]=NA
-
-Q=exp(colMeans(logQ,na.rm=T))
-
-
-
+logQ= 0.5*logS[i,] -
+     logn[i] +
+     1.66*logbeta[i] +
+     1.66*(log(r[i]/(r[i]+1))) +
+     ((1.66*r[i]) +1)*log(Wobs[i,])  
+    
+    
+finalQ[i,]=exp(logQ)
 }
 
+    
+finalQ[is.infinite(finalQ)]=NA
+    
+    
+finalQ=colMeans(finalQ,na.rm=TRUE) #take the mean for a given time t
+    #in linspace
+
+  
+return(finalQ)
+
+  
+}
 
 norm_to_lognorm=function(mu,sigma){
     lognorm_mu=2*log(mu)-0.5*log(mu^2+sigma^2)
     lognorm_sigma= log(mu^2 + sigma^2) - 2*(log(mu))
     return(list('mu'=lognorm_mu,'sigma'=lognorm_sigma))
-
+    
 }
-
-
-
 
 
 
